b22aa2b587c37fe6a96305cbef5ea0d6ba529968
arfjava <arrfjava@gmail.com>
2019-12-07 18:58:28 +0330

    class Utilities completed.

--- src/ir/maktab32/java/projects/homework5/arraylistanalysis/Utilities.java
+++ src/ir/maktab32/java/projects/homework5/arraylistanalysis/Utilities.java
@@ -1,4 +1,12 @@
 package ir.maktab32.java.projects.homework5.arraylistanalysis;
 
 public class Utilities {
+    public static boolean isNumeric(String str) {
+        try {
+            Long.parseLong(str);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
 }
